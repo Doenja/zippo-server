c5a20027ea0f215d9fef9f2a6e3ee42cd968011c
Doenja <doenjamaris@gmail.com>
2020-12-25 14:46:11 +0100

    Add logs

--- setState.js
+++ setState.js
@@ -5,19 +5,24 @@ const { exec } = require("child_process");
 
 const setState = (group, state, res) => {
     if (!group || typeof group !== 'string') {
+        console.log('Error: invalid group')
         return res.status(500).send({error: 'Invalid group'})
     }
     if (!state | (state !== 'ON' && state !== 'OFF' && state !== 'TOGGLE')) {
+        console.log('Error: invalid state')
         return res.status(500).send({error: 'Invalid state'})
     }
 
     exec(`mosquitto_pub -t zigbee2mqtt/${group}/set -m '{\"state\": \"${state}\"}' -d`, (error, stdout, stderr) => {
         if (error) {
-            return res.status(500).send({error: 'Command does not exist'})
+            console.log('Error: command not found')
+            return res.status(500).send({error: 'Command not found'})
         }
         if (stderr) {
-            return res.status(500).send({error: `Err: ${stderr}`})
+            console.log(`Error: ${stderr}`)
+            return res.status(500).send({error: stderr})
         }
+        console.log('State changed successfully', group, state)
         return res.status(200).send('State changed')
     });    
 } 
